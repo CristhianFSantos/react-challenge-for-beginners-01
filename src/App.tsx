0dc1a8d54ed6c07c34f0957b7842e745811d6c6b
Cristhian Santos <cristhianfelipe0@gmail.com>
2023-01-23 15:10:49 -0300

    ✨ feat: adds undo and redo features

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
-import { useState } from 'react'
-import './App.scss'
+import { useCallback, useState } from "react";
+import "./App.scss";
+import { CustomButtom } from "./components/CustomButton/CustomButton";
+import { Circle, Panel } from "./components/Panel/Panel";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [_, setCurrentCircle] = useState<Circle>();
+
+  const [circlesList, setCirclesList] = useState<Circle[]>([]);
+  const redoList: Circle[] = [];
+
+  const getCurrentCircle = useCallback((circle: Circle) => {
+    setCurrentCircle(circle);
+
+    setCirclesList((prevCircleItem: any) =>
+      prevCircleItem ? [...prevCircleItem, circle] : [circle]
+    );
+  }, []);
+
+  const onClickUndo = useCallback(() => {
+    setCirclesList((prevState: any) => {
+      const newState = [...prevState];
+      const lastItem = newState.pop();
+      redoList.push(lastItem);
+      return newState;
+    });
+  }, []);
+
+  const onClickRedo = useCallback(() => {
+    setCirclesList((prevState: any) => {
+      if (redoList.length > 0) {
+        const newState = [...prevState];
+        const lastItem = redoList.pop();
+        newState.push(lastItem);
+        return newState;
+      }
+      return prevState;
+    });
+  }, []);
 
   return (
     <div className="App">
-      
+      <Panel setCurrentCircle={getCurrentCircle} circlesList={circlesList} />
+
+      <div className="button-container">
+        <CustomButtom onClick={onClickUndo} label="Undo" />
+        <CustomButtom onClick={onClickRedo} label="Redo" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
